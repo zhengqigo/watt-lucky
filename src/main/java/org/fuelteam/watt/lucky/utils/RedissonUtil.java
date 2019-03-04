e788eb3ab41a069666add2fcf8d7c40da8a9d54f
zhengqigo <zhengqigo@vip.163.com>
2019-03-04 18:35:20 +0800

    tijiao

--- src/main/java/org/fuelteam/watt/lucky/utils/RedissonUtil.java
+++ src/main/java/org/fuelteam/watt/lucky/utils/RedissonUtil.java
@@ -2,17 +2,17 @@ package org.fuelteam.watt.lucky.utils;
 
 import java.util.concurrent.ExecutionException;
 
-import org.fuelteam.watt.lucky.utils.StringUtil;
 import org.redisson.api.RFuture;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RedissonUtil {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(RedissonUtil.class);
-    
-    public static String getAsync(RedissonClient redissonClient, String bucket) {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getAsync(RedissonClient redissonClient, String bucket, Class<T> clazz) {
         RFuture<Object> future = redissonClient.getBucket(bucket).getAsync();
         Object result = null;
         try {
@@ -20,6 +20,7 @@ public class RedissonUtil {
         } catch (InterruptedException | ExecutionException ex) {
             logger.error("getAsync exception", ex);
         }
-        return StringUtil.clean(result);
+        if (result == null) return null;
+        return (T) result;
     }
 }
