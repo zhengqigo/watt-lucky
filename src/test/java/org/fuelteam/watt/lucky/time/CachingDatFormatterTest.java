590703a18cd00f96b0d1b3f5a14f7ee990165cdb
zhengqigo <zhengqigo@vip.163.com>
2019-06-05 16:28:06 +0800

    tijiao

--- src/test/java/org/fuelteam/watt/lucky/time/CachingDatFormatterTest.java
+++ src/test/java/org/fuelteam/watt/lucky/time/CachingDatFormatterTest.java
@@ -4,23 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
 
-import org.fuelteam.watt.lucky.time.CachingDateFormatter;
-import org.fuelteam.watt.lucky.time.DateFormatUtil;
-import org.fuelteam.watt.lucky.utils.MoreDateUtil;
 import org.junit.Test;
 
 public class CachingDatFormatterTest {
 
     @Test
     public void test() {
-        Date date = MoreDateUtil.str2date("2016-11-01 12:23:44.000", "yyyy-MM-dd HH:mm:ss.SSS");
+        Date date = DateUtil.str2date("2016-11-01 12:23:44.000", "yyyy-MM-dd HH:mm:ss.SSS");
 
-        CachingDateFormatter formatter = new CachingDateFormatter(DateFormatUtil.PATTERN_DEFAULT);
+        CachingDateFormatter formatter = new CachingDateFormatter(DateUtil.PATTERN_DEFAULT);
         assertThat(formatter.format(date.getTime())).isEqualTo("2016-11-01 12:23:44.000");
         assertThat(formatter.format(date.getTime())).isEqualTo("2016-11-01 12:23:44.000");
         assertThat(formatter.format(date.getTime() + 2)).isEqualTo("2016-11-01 12:23:44.002");
 
-        CachingDateFormatter formatterOnSecond = new CachingDateFormatter(DateFormatUtil.PATTERN_DEFAULT_ON_SECOND);
+        CachingDateFormatter formatterOnSecond = new CachingDateFormatter(DateUtil.PATTERN_DEFAULT_ON_SECOND);
         assertThat(formatterOnSecond.format(date.getTime())).isEqualTo("2016-11-01 12:23:44");
         assertThat(formatterOnSecond.format(date.getTime())).isEqualTo("2016-11-01 12:23:44");
         assertThat(formatterOnSecond.format(date.getTime() + 2)).isEqualTo("2016-11-01 12:23:44");
