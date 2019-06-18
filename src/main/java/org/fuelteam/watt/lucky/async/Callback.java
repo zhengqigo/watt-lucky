30a39c6f3a83ce7c08c0c9c352c396258284196a
zhengqigo <zhengqigo@vip.163.com>
2019-06-18 14:05:21 +0800

    tijiao

--- src/main/java/org/fuelteam/watt/lucky/async/Callback.java
+++
@@ -1,8 +0,0 @@
-package org.fuelteam.watt.lucky.async;
-
-public interface Callback<T> {
-
-    public void onSuccess(T t);
-
-    public void onFailure(Exception ex);
-}
--- src/main/java/org/fuelteam/watt/lucky/parallel/IParallel.java
+++
@@ -1,6 +0,0 @@
-package org.fuelteam.watt.lucky.parallel;
-
-public interface IParallel<T> {
-
-    public void execute(T context);
-}
